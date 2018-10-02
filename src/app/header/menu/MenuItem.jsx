ccd19f201d1362bc7040d26595391aff099605a7
Roman Churganov <curdes@gmail.com>
2018-10-02 13:30:03 +0300

    Иконки в меню, док

--- src/app/header/menu/MenuItem.jsx
+++ src/app/header/menu/MenuItem.jsx
@@ -13,20 +13,29 @@ class MenuItem extends React.PureComponent {
     determineIcon() {
         const {itemInfo, classes, iconComponent} = this.props;
 
-        if (iconComponent) {// as Component
+        if (iconComponent) {
+            // Component из Material UI https://material.io/tools/icons
+            // название в snake_case, преобразовать в CamelCase
+            // можно создавать свои из SVG кодов
             return (
                 <this.props.iconComponent className={classes.dropdownIcons}/>
             );
 
         } else if (itemInfo.iconName) {
 
-            if (itemInfo.iconName.indexOf(" fa-") > 0) { // as Font Awesome https://fontawesome.com/icons
-                const parts = itemInfo.iconName.split(' ');
+            if (itemInfo.iconName.indexOf(" fa-") > 0) {
+                // as Font Awesome https://fontawesome.com/icons - PRO не работают
+                // пишутся как fas fa-... / fab fa-... / fal fa-... / far fa-...
 
-                return ( // fas fa-.../fab fa-.../fal fa-.../far fa-...
+                const parts = itemInfo.iconName.split(' ');
+                return (
                     <i className={classNames(parts[0], parts[1], classes.icon, classes.dropdownIcons)}/>
                 );
-            } else { // as Material UI icon https://material.io/tools/icons
+            } else {
+                // Material UI на основе Font Awesome https://material.io/tools/icons
+                // и https://materialdesignicons.com/ - работают не все
+                // названия с нижним подчеркиванием (_)
+
                 return (
                     <Icon className={classNames(classes.icon, classes.dropdownIcons)}>
                         {itemInfo.iconName}
