61cde3fbc355eba8860e497867c00f3353d1cd66
Roman Churganov <curdes@gmail.com>
2018-11-02 20:03:02 +0300

    Добавлен Spring Boot

---
+++ README.md
@@ -0,0 +1,17 @@
+
+#Команды
+    `build` - Сборка бэкенда
+
+#Первичная настройка
+    1. IntelliJ IDEA или др. IDE для работы с Java
+
+#Troubleshooting
+
+#Используется
+    - Java
+    - Gradle
+    - Spring Boot
+    - Spring Boot Gradle Plugin https://docs.spring.io/spring-boot/docs/2.0.5.RELEASE/gradle-plugin/reference/html/
+    - Spring Security https://spring.io/guides/gs/securing-web/
+
+
---
+++ backend/backend-webapi/src/main/java/ru/rich/matshop/webapi/Start.java
@@ -0,0 +1,46 @@
+package ru.rich.matshop.webapi;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import java.util.Arrays;
+
+@SpringBootApplication(scanBasePackageClasses = Start.class)
+@Import(WebSecurityConfig.class)
+public class Start {
+    private static Logger log = LoggerFactory.getLogger(Start.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Start.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            if (false) logBeans(ctx);
+            if (true) logBaseUrl();
+        };
+    }
+
+    private void logBeans(ApplicationContext ctx) {
+        log.info("Let's inspect the beans provided:");
+        String[] beanNames = ctx.getBeanDefinitionNames();
+
+        Arrays.sort(beanNames);
+        Arrays.asList(beanNames).forEach(x -> {
+            log.info("   -> " + x);
+        });
+    }
+
+    private void logBaseUrl() {
+        log.info("Tomcat started at http://127.0.0.1:8080");
+    }
+}
