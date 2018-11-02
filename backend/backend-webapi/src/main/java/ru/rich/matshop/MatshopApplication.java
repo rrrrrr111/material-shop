ac003c83b08de2bf4094f0f26fe58f7fb7bd5111
Roman Churganov <curdes@gmail.com>
2018-11-02 17:29:27 +0300

    Добавлены gradle и Java

---
+++ backend/backend-webapi/src/main/java/ru/rich/matshop/MatshopApplication.java
@@ -0,0 +1,12 @@
+package ru.rich.matshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatshopApplication.class, args);
+	}
+}
