5e034d9450ed02226db5ba96fa61cc590201eeff
Roman Churganov <RIChurganov.SBT@sberbank.ru>
2018-11-24 15:15:29 +0300

    DbSeed, bugfix

--- backend/backend-db/src/test/resources/ru/rich/matshop/db/seed/db-seed-products.sql
+++ backend/backend-db/src/test/resources/ru/rich/matshop/db/seed/db-seed-products.sql
@@ -5,3 +5,8 @@ VALUES (1, {TS '2000-01-01 11:11:11'}, 'ACTIVE', '{}', 'PLUS(30%)', 1000, 'JSON_
        (1, {TS '2000-01-01 11:11:11'}, 'ACTIVE', '{}', 'PLUS(30%)', 1000, 'JSON_1.0'),
        (1, {TS '2000-01-01 11:11:11'}, 'ACTIVE', '{}', 'PLUS(30%)', 1000, 'JSON_1.0'),
        (1, {TS '2000-01-01 11:11:11'}, 'ACTIVE', '{}', 'PLUS(30%)', 1000, 'JSON_1.0');
+
+INSERT INTO PRODUCT_COSMETIC (product_id, name, price, category, popularity)
+    (SELECT ID, '', BASE_PRICE, 1, 0
+     FROM PRODUCT
+     WHERE EDIT_DATE = {TS '2000-01-01 11:11:11'});
